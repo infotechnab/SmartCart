a7fff395b09266c7eedb6aa0e5b7034701c04a05
bhomnath <bhomnath@outlook.com>
2014-06-23 10:42:22 +0545

    unwanted lines removed

--- application/controllers/cartdetails.php
+++ application/controllers/cartdetails.php
@@ -117,7 +117,6 @@ class Cartdetails extends CI_Controller {
 
 
             foreach ($cart as $item) {
-                var_dump($item);
                 if ($item) {
                     mysql_query("INSERT INTO `product_oder_detail` (o_id,p_id,qty,trans_id,trans_num) 
        VALUES ('" . $oId . "','" . $item['id'] . "', '" . $item['qty'] . "', '$tid', '$tr')");
@@ -132,86 +131,13 @@ class Cartdetails extends CI_Controller {
 
     function insert_cart_item() {
         $this->load->model('dbmodel');
-        $this->load->helper('form');
-        $this->load->library(array('form_validation', 'session'));
         
-
-        $this->form_validation->set_rules('u_fname', 'First name', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,15}$/]|required|xss_clean|max_length[15]');
-        $this->form_validation->set_rules('u_lname', 'Last name', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,15}$/]|required|xss_clean|max_length[15]');
-        $this->form_validation->set_rules('street_address', 'Address', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-        $this->form_validation->set_rules('Town_address', 'City/Town', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-        $this->form_validation->set_rules('District_address', 'State/District', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-        $this->form_validation->set_rules('zip', 'Zip', 'trim|regex_match[/^[0-9]{5,15}$/]|required|xss_clean|max_length[15]');
-        $this->form_validation->set_rules('country', 'Country', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-        $this->form_validation->set_rules('u_contact', 'Contact no.', 'trim|regex_match[/^[0-9]{5,15}$/]|required|xss_clean|max_length[15]');
-        $this->form_validation->set_rules('user_email', 'Email', 'trim|regex_match[/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/]|required|xss_clean|max_length[200]');
-
         
 
         /* if shipping enabled */
-        if (shipenabled and different) {
+        
 
-            $this->form_validation->set_rules('s_fname', 'First name', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,15}$/]|required|xss_clean|max_length[15]');
-            $this->form_validation->set_rules('s_lname', 'Last name', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,15}$/]|required|xss_clean|max_length[15]');
-            $this->form_validation->set_rules('s_address', 'Address', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-            $this->form_validation->set_rules('c_city', 'City', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-            $this->form_validation->set_rules('s_state', 'State', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-            $this->form_validation->set_rules('s_zip', 'Zip', 'trim|regex_match[/^[0-9]{5,15}$/]|required|xss_clean|max_length[15]');
-            $this->form_validation->set_rules('s_country', 'Country', 'trim|regex_match[/^[a-z,0-9,A-Z]{5,35}$/]|required|xss_clean|max_length[35]');
-            $this->form_validation->set_rules('s_contact', 'Contact no.', 'trim|regex_match[/^[0-9]{5,15}$/]|required|xss_clean|max_length[15]');
-            $this->form_validation->set_rules('s_email', 'Email', 'trim|regex_match[/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/]|required|xss_clean|max_length[50]');
-         
-                
-        } else {
-            $shipfname = $fname;
-            $shiplname = $lname;
-            $shipstreet = $street;
-            $shiptown = $town;
-            $shipdistrict = $district;
-            $shipzip = $zip;
-            $shipcountry = $country;
-            $shipcontact = $contact;
-            $shipemail = $email;
-        }
-
-        if ($this->form_validation->run() == FALSE ) {
-            $data['user_validation_message'] = validation_errors();
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/navigation');
-            $this->load->view('templates/userRegistrationAndShipping', $data);
-            $this->load->view('templates/footer');
-        } else {
-            
-            /* here are all inputed fields*/
-            $fname = $this->input->post('u_lname');
-        $lname = $this->input->post('u_lname');
-        $street = $this->input->post('street_address');
-        $town = $this->input->post('Town_address');
-        $district = $this->input->post('District_address');
-        $zip = $this->input->post('zip');
-        $country = $this->input->post('country');
-        $contact = $this->input->post('u_contact');
-        $email = $this->input->post('user_email');
-                $shipfname = $this->input->post('s_fname');                        
-                $shiplname = $this->input->post('s_lname');                         
-                $shipstreet = $this->input->post('s_address');                        
-                $shiptown = $this->input->post('c_city');                         
-                $shipdistrict = $this->input->post('s_state');                          
-                $shipzip = $this->input->post('s_zip');                          
-                $shipcountry = $this->input->post('s_country');                       
-                $shipcontact = $this->input->post('s_contact');                         
-                $shipemail = $this->input->post('s_email');
-                
-            
-            $this->dbmodel->add_new_user($name, $fname, $lname, $email, $pass, $status, $user_type, $contact, $address);
-
-
-            $this->productmodel->add_new_user($username, $fname, $lname, $email, $pass, $contact, $address, $city, $state, $country, $zip);
-            $lastuser = $this->productmodel->get_last_user();
-            foreach ($lastuser as $userId) {
-                $uid = $userId->id;
-            }
-
+      
             $this->productmodel->order_user($s_username, $s_address, $s_city, $s_state, $s_country, $s_zip, $s_email, $s_contact, $uid);
             $orderId = $this->productmodel->get_last_order();
             foreach ($orderId as $oid) {
@@ -242,7 +168,7 @@ class Cartdetails extends CI_Controller {
 
             $this->load->view('templates/inserted');
         }
-    }
+    
 
     function display() {
         if ($_POST) { //Post Data received from product list page.
